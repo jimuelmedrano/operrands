3341b44a30ffaf4c4fc0817e99c43e79b7d73fef
Jimuel Renzo Medrano <42688255+jimuelmedrano@users.noreply.github.com>
2024-10-03 23:02:42 +0800

    Update AnnouncementBanner.tsx

--- src/components/AnnouncementBanner.tsx
+++ src/components/AnnouncementBanner.tsx
@@ -1,6 +1,6 @@
 const AnnouncementBanner = () => {
   return (
-    <div className="absolute bottom-0 left-0 w-full h-5 bg-primaryDark flex-center">
+    <div className="fixed bottom-0 left-0 w-full h-5 bg-primaryDark flex-center">
       <div className="flex-center">
         <span className="text-xs dark:text-black">
           Note: This website is currently a work in progress.
